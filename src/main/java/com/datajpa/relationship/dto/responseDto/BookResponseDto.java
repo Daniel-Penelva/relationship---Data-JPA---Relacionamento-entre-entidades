ed11be2e0003c6565d0a6e8ac1a3f6b859db633d
Daniel Penelva <d4n.andrade@gmail.com>
2023-12-11 20:36:15 -0300

    dto - criando os responseDto do Author, Book e Category

---
+++ src/main/java/com/datajpa/relationship/dto/responseDto/BookResponseDto.java
@@ -0,0 +1,16 @@
+package com.datajpa.relationship.dto.responseDto;
+
+import java.util.List;
+
+import lombok.Data;
+
+@Data
+public class BookResponseDto {
+
+    private Long id;
+    private String name;
+    private List<String> authorNames;
+    private String categoryName;
+
+    
+}
